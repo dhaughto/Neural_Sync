1f5ab651bb2ec283bad920384ae54252dc789956
Dillon Haughton <dhaughto@ucsd.edu>
2019-03-06 21:20:59 -0800

    multiple_nodes

--- Kuramoto.py
+++ Kuramoto.py
@@ -141,11 +141,11 @@ def multiple_oscillator(N,K,t):
 		sums = []
 		for i in range(len(theta)):
 			sums += [np.exp(np.complex(0,theta[i]))]
-			sums = sum(sums)
+		sums = sum(sums)
 	
 		side = (1/N)*(sums/(np.exp(complex(0,average))))
 	
-		return abs(side.real)		# Not sure if i can put the abs here though but it
+		return side.real		# Not sure if i can put the abs here though but it
 									# seems to be an accurate description
 
 	coupling = [r(i) for i in form_at]
@@ -181,41 +181,26 @@ print(elapsed_time)
 '''
 # 
 #---------------------------------------
-#			Multiple Nodes
+#	Two Nodes/ Two Oscilator per node
 #---------------------------------------
 
-def multiple_nodes(N,P,C,t):
+def two_nodes(C,t):
 	print()
-	# N = number of oscillators
-	# P = number of nodes
-	# K = coupling constant
-	# C = weighted global constant
-	# t (tuple of time)
-	#----------------------------
-	# Actually Modeling Epilepsy
-	#----------------------------
-	# 6 is an abritrary number of nodes P
-	# p goes from 1 - P
-	# i goes from 1 - N
-	# q goes from 1 - P
-
-	# Omega is a matrix N columns, P rows
-	# K list P long
-	# C is an integer
-	
-	omega = np.random.uniform(0.1,3,size=(P,N))
-	# Connection Matrix at random
-	conn = np.random.choice([0,1],p = [0.2,0.8],size=(P,P))
+	omega = np.random.uniform(0.1,3,size=(1,4))[0]
+	# Connection Matrix at random but not needed for example of two
+	conn = np.random.choice([0,1],p = [0.0,1.0],size=(2,2))
 	np.fill_diagonal(conn,0)
 	# Iniitial conditions
-	y0 = np.random.uniform(0,np.pi,size = (P,N))
+	y0 = np.random.uniform(0,np.pi,size=(1,4))[0]
+	print('Initial Conditions: ')
 	print(y0)
+	print()
 	print('Connectivity Matrix Randomly Generated:')
 	print(conn)
 	# Random K values
 	print()
 	print('K Values Randomly generated:')
-	K = np.random.uniform(0.1,6,size=(P,1))
+	K = np.random.uniform(0.1,6,size=(1,2))[0]
 
 	print(K)
 
@@ -226,35 +211,182 @@ def multiple_nodes(N,P,C,t):
 
 	# Need to use reduced version
 
-	def kurakura(t,theta):
-		# initialize matrix
-		dthetadt = np.empty((P,N))
+	def kuratwo(t,theta):
+		dthetadt = [omega[0] + (K[0]/2)*np.sin(theta[1] - theta[0])+C*(conn[0,1]/2)*(np.sin(theta[2]-theta[0])+np.sin(theta[3]-theta[0])),
+					omega[1] + (K[0]/2)*np.sin(theta[0] - theta[1])+C*(conn[0,1]/2)*(np.sin(theta[2]-theta[1])+np.sin(theta[3]-theta[1])),
+					omega[2] + (K[1]/2)*np.sin(theta[3] - theta[2])+C*(conn[1,0]/2)*(np.sin(theta[0]-theta[2])+np.sin(theta[1]-theta[2])),
+					omega[3] + (K[1]/2)*np.sin(theta[2] - theta[3])+C*(conn[1,0]/2)*(np.sin(theta[0]-theta[3])+np.sin(theta[1]-theta[3]))]
+		return dthetadt
+		
+	sol = solve_ivp(kuratwo,t,y0)
+	r = [1,1]
 
-		for p in range(P):
-
-			part_1 = []
-
-			for i in range(N):
-				part_2 = []
-				part_3 = []
-				part_4 = []
-				for q in range(P):
-
-					for j in range(N):
-						# Dont want this here for sure
-						part_2 += [np.sin(theta[p,j] - theta[p,i])]
-						part_3 += [(conn[p,q]/N)*np.sin(theta[q,j] - theta[p,i])]
-
-				part_1 += [omega[p,i] + (K[p,0]/N)* sum(part_2) + C * sum(part_3)]
-
-			dthetadt = np.vstack([dthetadt,part_1])
-		return dthetadt	 		
-
-
-	sol = odeint(kurakura,y0,t)
-	print(sol)	
-
-multiple_nodes(100,3,2.0,(0,20))	
+	node_1 = []
+	for i in range(len(sol.y[0])):
+		node_1 += [[sol.y[0][i],sol.y[1][i]]]
 	
 
+	node_2 = []
+	for i in range(len(sol.y[0])):
+		node_2 += [[sol.y[2][i],sol.y[3][i]]]
+
+# Animation Portion
+#------------------------------------------
+
+	fig = plt.figure()
+	ax = plt.subplot(211, polar=True)
+
+	point, = ax.plot(node_1[0],r,'go')
+
+	def ani(i):
+		point.set_data([node_1[i]],[r])
+		return point
+
+	
+	ani = animation.FuncAnimation(fig,ani,frames = len(sol.y[0]) ,interval = 500)
+
+	ax = plt.subplot(212,polar=True)
+
+	line, = ax.plot(node_2[0],r,'go')
+
+	def ani1(i):
+		line.set_data([node_2[i]],[r])
+		return line
+
+	ani1 = animation.FuncAnimation(fig,ani1,frames=len(sol.y[0]),interval=500)	
+
+	plt.show()			
+
+#--------------------------------------------
+#---------------------------------------
+#  Multiple Nodes/ Multiple Oscillators
+#---------------------------------------	
+
+# This is too slow but finish this code then in new sheet can 
+# try to make it faster
+
+
+
+def multiple_nodes(P,N,C,t):
+	# Omega Values
+	omega = np.random.uniform(0.1,3,size=(1,P*N))[0]
+	# Connectivity Matrix
+	conn = np.random.choice([0,1],p = [0.4,0.6],size=(P,P))
+	np.fill_diagonal(conn,0)
+	# Flatten out Conn Matrix
+	new_conn = []
+	for i in conn:
+		for j in range(N):
+			new_conn += [i]
+	conn = np.asarray(new_conn)
+	# Initial Conditions
+	y0 = np.random.uniform(0,np.pi,size=(1,P*N))[0]
+	# K values extended for loop
+	K = np.random.uniform(0.1,3.5,size=(1,P))[0]
+	new_K = []
+	for i in K:
+		for j in range(N):
+			new_K += [i] 
+	K = np.asarray(new_K)			
+
+	# Versatile Kuramoto Equation
+	def kura_node(t,theta):
+		#---------------------------------------------
+		dthetadt = []
+		for i in range(P*N):
+			
+			sums_1 = 0
+			
+			for j in range(P*N):
+				sums_1 += np.sin(theta[j] - theta[i])
+				
+			
+			sums_con = 0
+			
+			for q in range(P-1):
+				for m in range(P*N):
+					sums_con += (conn[i,q]/N)*np.sin(theta[m] - theta[i])
+					
+			dthetadt += [omega[i] + ((K[i]/N)*sums_1) + C * sums_con]
+		return dthetadt
+		#---------------------------------------------
+
+	sol = solve_ivp(kura_node,t,y0)
+
+	r = [1] * N
+
+	x = np.vsplit(sol.y,P)
+	
+	# Animation Portion of Code
+	#---------------------------------------------
+	# First Node
+	
+	fig = plt.figure()
+	ax = plt.subplot(321, polar=True)
+	scat1 = ax.scatter(x[0][:,0],r)
+
+	def ani1(i):
+		scat1.set_offsets(np.c_[x[0][:,i],r])
+		return scat1,
+
+	ani1 = animation.FuncAnimation(fig,ani1,frames = len(sol.t),interval = 500)
+	#---------------------------------------------
+	# Second Node
+	ax = plt.subplot(322,polar=True)
+	scat2 = ax.scatter(x[1][:,0],r)
+
+	def ani2(i):
+		scat2.set_offsets(np.c_[x[1][:,i],r])
+		return scat2,
+
+	ani2 = animation.FuncAnimation(fig,ani2,frames = len(sol.t),interval = 500)
+	#---------------------------------------------
+	#Third Node
+	ax = plt.subplot(323,polar=True)
+	scat3 = ax.scatter(x[2][:,0],r)
+
+	def ani3(i):
+		scat3.set_offsets(np.c_[x[2][:,i],r])
+		return scat3,
+
+	ani3 = animation.FuncAnimation(fig,ani3,frames = len(sol.t),interval = 500)
+	#---------------------------------------------
+	# Fourth Node
+	ax = plt.subplot(324,polar=True)
+	scat4 = ax.scatter(x[3][:,0],r)
+
+	def ani4(i):
+		scat4.set_offsets(np.c_[x[3][:,i],r])
+		return scat4,
+
+	ani4 = animation.FuncAnimation(fig,ani4,frames = len(sol.t),interval = 500)
+	#---------------------------------------------
+	# Fifth Node
+	ax = plt.subplot(325,polar=True)
+	scat5 = ax.scatter(x[4][:,0],r)
+
+	def ani5(i):
+		scat5.set_offsets(np.c_[x[4][:,i],r])
+		return scat5,
+
+	ani5 = animation.FuncAnimation(fig,ani5,frames = len(sol.t),interval = 500)
+	#---------------------------------------------
+	# Sixth Node
+	ax = plt.subplot(326,polar=True)
+	scat6 = ax.scatter(x[5][:,0],r)
+
+	def ani6(i):
+		scat6.set_offsets(np.c_[x[5][:,i],r])
+		return scat6,
+
+	ani6 = animation.FuncAnimation(fig,ani6,frames = len(sol.t),interval = 500)
+
+	plt.show()
+
+
+		
+start_time = time.time()	
+multiple_nodes(6,5,6,(0,20))
+elapsed_time = time.time() - start_time
+print(elapsed_time)
 
