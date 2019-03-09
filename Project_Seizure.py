023636843d7a730086e4c03deced80e79f670a24
Dillon Haughton <dhaughto@ucsd.edu>
2019-03-09 13:49:37 -0800

    Presentation ready

--- Project_Seizure.py
+++ Project_Seizure.py
@@ -13,49 +13,322 @@ import time
 # Plan for Demo. Demonstrate two oscilator Kuramoto Model
 # Demonstrate multiple Oscillators and plot coherence as a function of time
 # Demonstrate three nodes and plot signal next to them
-np.random.seed(100)
-N = 3
-t = (0,1000)
-omega = np.array([uniform(1,3) for i in range(N)])
-y0 = np.array([uniform(0,2*np.pi) for i in range(N)])
+def two_oscillator(K,y0_range,omega_range,t):
+	# Give two initial coniditons in list
+	# Give time in tupal
+	# Give list of two omega values
+#---------------------------------------
+#             Two Oscillators          |
+#---------------------------------------
+	omega_range_love = omega_range[0]
+	omega_range_high = omega_range[1]
+	omega = [uniform(omega_range_love,omega_range_high) for i in range(2)]
+	y0 = [uniform(y0_range[0],y0_range[1]) for i in range(2)]
 
-K = 3
+	def kura2ode(t,y):
+		dydt = [omega[0] + K*np.sin(y[1]-y[0])/2, omega[1] + K*np.sin(y[0]-y[1])/2]
+		return dydt
+
+	sol = solve_ivp(kura2ode,t,y0)
+
+	r = [1] * len(sol.t)
+	theta1 = sol.y[0]
+	theta2 = sol.y[1]
+
+	fig = plt.figure()
+	ax = plt.subplot(111, polar=True)
+
+
+	point, = ax.plot(theta1[0],r[0],'go')
+	line, = ax.plot(theta2[0],r[0],'go')
+
+	def ani(coords):
+		point.set_data([coords[0]],[coords[1]])
+		line.set_data([coords[2]],[coords[3]])
+		return point, line
+
+	def frames():
+		for k,j,x,z in zip(theta1,r,theta2,r):
+			yield k,j,x,z	
+
+	
+	ani = animation.FuncAnimation(fig,ani,frames = frames,interval = 800,repeat=False)
+	plt.title('Two Synced Oscillators: K value of {}'.format(K))
+	plt.show()
+
+
+
+#two_oscillator(2.2,[0,np.pi],[0,0.5],(0,30))
+#===========================================================================================
+
+def multiple_oscillator(N,K,y0_range,omega_range,t):
+# Number of Oscillators
+# Speed things up by adding np arrays wherever you can
+# time increased drastically at N = 1000 need to fix
+
+#----------------------------
+#     Table of Observation
+# K = 6.8 Sync then out of sync
+# omenga (1,5)
+# y0 (0,2pi)
+
+	omega = np.array([uniform(omega_range[0],omega_range[1]) for i in range(N)],dtype=np.float16)
+	y0 = np.array([uniform(y0_range[0],y0_range[1]) for i in range(N)],dtype=np.float16)
+	
 
 # Need to streamlin this portion.... lot of computation
-def kuraN(t,theta):
-	dthetadt = []
-	for i in range(N):
-		sums = 0 
+	def kuraN(t,theta):
+		dthetadt = []
+	
+		for i in range(N):
+			sums = 0 
+			for j in range(N):
+				sums += np.sin(theta[j] - theta[i])
+			dthetadt += [omega[i] + (K/N)*(sums)]
+
+		return dthetadt	
+
+	sol = solve_ivp(kuraN,t,y0)
+
+# You want list of each time slice
+# add to list sol.y[i][0] next list sol.y[i][1]
+# Need to work the data here for animation
+	form_at = []
+	for i in range(len(sol.y[0])):
+		form_half = []	
+		form_half += [sol.y[j][i] for j in range(N)]
+		form_at += [form_half]
+
+
+	r = [1] * N
+	rl = [r] * len(sol.t)
+
+	fig = plt.figure()
+	ax = plt.subplot(121, polar=True)
+
+#---------------------------------------
+# Want a time label so you can see what time there is sync
+# Also need animation of coherence as well
+	point, = ax.plot(form_at[0],rl[0],'go')
+
+	def ani(i):
+		point.set_data([form_at[i]],[rl[i]])
+	
+		return point
+	
+	ani = animation.FuncAnimation(fig,ani,frames = len(sol.t),interval = 200,repeat=False)
+	plt.title('{} Oscillators'.format(N))
+
+#---------------------------------------
+#				Coherence
+#---------------------------------------
+
+#? Email Professor ask about coherence 
+	def r(theta):
+	
+		N = len(theta)
+		average = np.mean(theta)
+	
+	
+		sums = []
+		for i in range(len(theta)):
+			sums += [np.exp(np.complex(0,theta[i]))]
+		sums = np.sum(sums)
+	
+		side = (1/N)*(sums/(np.exp(complex(0,average))))
+	
+		return abs(side)		# Not sure if i can put the abs here though but it
+									# seems to be an accurate description
+	coupling = []								
+	for i in form_at:
+		coupling += [r(i)]
+									
+
+	ax1 = plt.subplot(122)
+
+	line, = ax1.plot(sol.t[0],coupling[0],'-o',markersize=2)
+
+	def ani2(i):
+		line.set_data(sol.t[:i],coupling[:i])
+		line.axes.axis([0,max(sol.t),0,1])
+
+		return line
+
+	ani2 = animation.FuncAnimation(fig,ani2,frames = len(sol.t),interval=200,repeat=False)	
+
+	plt.title('Coherence')
+	plt.show()	
+		
+#multiple_oscillator(100,2.4,[0,2*np.pi],[0.1,3],(0,50))
+#===========================================================================================
+
+def multiple_nodes(P,N,C,omega_range,K_range,y0_range,t):
+	start_time = time.time()
+	# Omega Values
+	omega = np.random.uniform(omega_range[0],omega_range[1],size=(1,P*N))[0]
+	# Connectivity Matrix
+	conn = np.random.choice([0,1],p=[0.4,0.6],size=(P,P))
+	np.fill_diagonal(conn,0)
+	print("Connectivity Matrix:")
+	print(conn)
+	print()
+
+	new_conn = []
+	for i in conn:
 		for j in range(N):
-			sums += np.sin(theta[j] - theta[i])
-		dthetadt += [omega[i] + (K/N)*(sums)]
-	return dthetadt	
+			new_conn += [i]
+	conn = np.asarray(new_conn)
+	#Initial Conditions
+	y0 = np.random.uniform(y0_range[0],y0_range[1],size=(1,P*N))[0]
+	# K Values
+	K = np.random.uniform(K_range[0],K_range[1],size=(1,P))[0]
+	print('K Values')
+	print(K)
+	print()
+	new_K = []
+	for i in K:
+		for j in range(N):
+			new_K += [i] 
+	K = np.asarray(new_K)
+
+	# Versatile Kuramoto Equation
+	def kura_node(t,theta):
+		#---------------------------------------------
+		dthetadt = []
+		for i in range(P*N):
+			
+			sums_1 = 0
+			
+			for j in range(P*N):
+				sums_1 += np.sin(theta[j] - theta[i])
+				
+			
+			sums_con = 0
+			
+			for q in range(P-1):
+				for m in range(P*N):
+					sums_con += (conn[i,q]/N)*np.sin(theta[m] - theta[i])
+					
+			dthetadt += [omega[i] + ((K[i]/N)*sums_1) + C * sums_con]
+		return dthetadt
+		#---------------------------------------------
+	sol = solve_ivp(kura_node,t,y0)
+
+	r = [1] * N
+
+	x = np.vsplit(sol.y,P)
+
+	#------------------------------
+	#     Calculate Coherenece
+	#------------------------------
+	def coherence(theta):
+		N = len(theta)
+		average = np.mean(theta)
+
+		sums = []
+		for i in range(len(theta)):
+			sums+=[np.exp(np.complex(0,theta[i]))]
+		sums = np.sum(sums)
+		
+		side = (1/N)*(sums/(np.exp(complex(0,average))))
+
+		return abs(side)
+
+		
+	coupling1 = []
+	coupling2 = []
+	coupling3 = []
+	for i in range(len(sol.t)):
+		coupling1 += [coherence(x[0][:,i])]
+		coupling2 += [coherence(x[1][:,i])]
+		coupling3 += [coherence(x[2][:,i])]
 
 	
-def kuraNpr(t,theta):
-	return omega + (K/N)*np.sum(np.sin(y0[:,None]-y0),axis=0)
-
-print(kuraN(t,y0))
-print(kuraNpr(t,y0))	
-
-sol1 = solve_ivp(kuraN,t,y0)
-sol2 = solve_ivp(kuraNpr,t,y0)
-
-print(kuraN(t,y0))
-print(kuraNpr(t,y0))
-
-print(sol1)
-print(sol2)
-
-
-
-
-
 	
+	#--------------------------------------------------
+	#                Animation Section
+	#--------------------------------------------------
+	# Animation 1
+	fig = plt.figure()
+	ax = plt.subplot(321,polar=True)
+	scat1 = ax.scatter(x[0][:,0],r)
+
+	def ani1(i):
+		scat1.set_offsets(np.c_[x[0][:,i],r])
+		return scat1,
+
+	ani1 = animation.FuncAnimation(fig,ani1,frames=len(sol.t),interval = 100, repeat=False)
+
+	plt.title('Nodes')
+	# Animation 2
+	ax = plt.subplot(322)
+
+	ax.set_ylim(-1,1)
+	sign, = ax.plot(sol.t,coupling1[0]*np.sin(sol.t - np.mean(x[0][:,0])))
+
+	def ani2(i):
+		sign.set_data([sol.t],[coupling1[i]*np.sin(sol.t - np.mean(x[0][:,i]))])
+		return sign,
+
+	ani2 = animation.FuncAnimation(fig,ani2,frames=len(coupling1),interval = 100, repeat=False)
+
+	plt.title('Generalized EEG')
+	# Animation 3
+	ax = plt.subplot(323,polar=True)
+	scat2 = ax.scatter(x[1][:,0],r)
+
+	def ani3(i):
+		scat2.set_offsets(np.c_[x[1][:,i],r])
+		return scat2,
+
+	ani3 = animation.FuncAnimation(fig,ani3,frames=len(sol.t),interval = 100, repeat=False)	
+
+	# Animation 4
+	ax = plt.subplot(324)
+	ax.set_ylim(-1,1)
+	sign2, = ax.plot(sol.t,coupling2[0]*np.sin(sol.t - np.mean(x[1][:,0])))
+
+	def ani4(i):
+		sign2.set_data([sol.t],[coupling2[i]*np.sin(sol.t - np.mean(x[1][:,i]))])
+		return sign2,
+
+	ani4 = animation.FuncAnimation(fig,ani4,frames=len(coupling2),interval=100,repeat=False)	
 
 
+	# Animation 5
+	ax = plt.subplot(325,polar=True)
+	scat3 = ax.scatter(x[2][:,0],r)
 
+	def ani5(i):
+		scat3.set_offsets(np.c_[x[2][:,i],r])
+		return scat3,
+
+	ani5 = animation.FuncAnimation(fig,ani5,frames=len(sol.t),interval=100,repeat=False)
+
+	# Animation 6
+	ax = plt.subplot(326)
+	ax.set_ylim(-1,1)
+	sign3, = ax.plot(sol.t,coupling3[0]*np.sin(sol.t - np.mean(x[2][:,0])))
+
+	def ani6(i):
+		sign3.set_data([sol.t],[coupling3[i]*np.sin(sol.t - np.mean(x[2][:,i]))])
+		return sign3,
+
+	ani6 = animation.FuncAnimation(fig,ani6,frames=len(coupling3),interval=100,repeat=False)	
 	
+	elapsed_time = time.time() - start_time
+	print('Time: ',elapsed_time)
+	plt.show()		 	
+
+
+
+# 3 Nodes, 10 each about 6~8 seconds
+# 3 Nodes, 20 each about 30 seconds
+# 3 Nodes, 100 each about 300 seconds (5 minutes)
+# 3 Nodes , 100 each for 30 seconds about 1000 seconds (15 minutes)
+multiple_nodes(3,10,3.3,[3.0,5.5],[4.5,3.6],[0,2*np.pi],(0,30))
+
+
 
 
 
