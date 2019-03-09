3cb00f7cb09c6cd8d961cdc47f21d92396b5d3d7
Dillon Haughton <dhaughto@ucsd.edu>
2019-03-09 13:53:02 -0800

    Potential Improvements to the code

---
+++ improvements.py
@@ -0,0 +1,50 @@
+
+
+#            Import Module
+#--------------------------------------------
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib import animation
+from scipy.integrate import solve_ivp, odeint
+from random import uniform
+import time
+
+
+# Plan for Demo. Demonstrate two oscilator Kuramoto Model
+# Demonstrate multiple Oscillators and plot coherence as a function of time
+# Demonstrate three nodes and plot signal next to them
+np.random.seed(100)
+N = 10
+t = (0,50)
+omega = np.array([uniform(1,3) for i in range(N)])
+y0 = np.array([uniform(0,2*np.pi) for i in range(N)])
+
+K = 8
+
+# Need to streamlin this portion.... lot of computation
+
+# These two are the same
+def kuraN(t,theta):
+	dthetadt = []
+	for i in range(N):
+		sums = 0 
+		for j in range(N):
+			sums += np.sin(theta[j] - theta[i])
+		dthetadt += [omega[i] + (K/N)*(sums)]
+	return dthetadt	
+
+	
+def kuraNpr(t,theta):
+	return omega + (K/N)*np.sum(np.sin(y0[:,None]-y0),axis=0)
+
+print(kuraN(t,y0))
+print(kuraNpr(t,y0))	
+
+sol1 = solve_ivp(kuraN,t,y0)
+sol2 = solve_ivp(kuraNpr,t,y0)
+
+print(sol1.y[0])
+print(sol2.y[0])
+print(len(sol1.y[0]))
+print(len(sol2.y[0]))
+
